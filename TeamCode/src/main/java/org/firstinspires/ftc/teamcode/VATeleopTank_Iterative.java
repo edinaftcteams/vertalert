7617c0c1133527e11b26d74f70bc5b52035a6a4b
Cole Brazel <cole.brazel@comcast.net>
2018-10-16 12:05:01 -0500

    if else statement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VATeleopTank_Iterative.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VATeleopTank_Iterative.java
@@ -141,13 +141,13 @@ public class VATeleopTank_Iterative extends OpMode{
         if (gamepad1.dpad_up) {
             robot.armstringspooler.setPower(robot.ARM_UP_POWER);
         } else {
-            robot.armstringspooler.setPower(0.5);
+            robot.armstringspooler.setPower(0.0);
         }
 
         if (gamepad1.dpad_down) {
             robot.armstringspooler.setPower(robot.ARM_DOWN_POWER);
         } else {
-            robot.armstringspooler.setPower(0.5);
+            robot.armstringspooler.setPower(0.0);
         }
 
             // Send telemetry message to signify robot running;
