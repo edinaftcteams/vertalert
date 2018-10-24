7ec0fd41edf42e0ef9f45a71357b39fe12c30189
Cole Brazel <cole.brazel@comcast.net>
2018-10-24 09:21:06 -0500

    un disabling

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VAPushbotEncoderAutonomousCrater.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VAPushbotEncoderAutonomousCrater.java
@@ -63,7 +63,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  */
 
 @Autonomous(name="Pushbot: Auto Drive By Encoder", group="Pushbot")
-@Disabled
+
 public class VAPushbotEncoderAutonomousCrater extends LinearOpMode {
 
     /* Declare OpMode members. */
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VAPushbotEncoderAutonomousDepot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VAPushbotEncoderAutonomousDepot.java
@@ -65,7 +65,7 @@ import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
  */
 
 @Autonomous(name="Pushbot: Auto Drive By Encoder", group="Pushbot")
-@Disabled
+
 public class VAPushbotEncoderAutonomousDepot extends LinearOpMode {
 
     /* Declare OpMode members. */
