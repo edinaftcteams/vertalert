86df36dd29e368cdde93f8d08153c715485211f0
Ahshaam <Ahshaam.m.abu@gmail.com>
2018-11-06 19:23:43 -0600

    run using encoder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VAPushbotEncoderAutonomousDepot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VAPushbotEncoderAutonomousDepot.java
@@ -171,7 +171,7 @@ public class VAPushbotEncoderAutonomousDepot extends LinearOpMode {
             // its target position, the motion will stop.  This is "safer" in the event that the robot will
             // always end the motion as soon as possible.
             // However, if you require that BOTH motors have finished their moves before the robot continues
-            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.]
             while (opModeIsActive() &&
                    (runtime.seconds() < timeoutS) &&
                    (robot.leftbackdrive.isBusy() && robot.rightbackdrive.isBusy() && robot.leftfrontdrive.isBusy() && robot.rightfrontdrive.isBusy())) {
